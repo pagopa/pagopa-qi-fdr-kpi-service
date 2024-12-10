4b353be3970f57c82284547fa0d5d44990c0ec93
Dylan Tangredi <dylan.tangredi@bvtech.com>
2024-12-10 15:27:37 +0100

    [PPABV-83] feat: add project setup with spring initializr

---
+++ src/main/java/it/pagopa/qi/fdrkpiservice/PagopaQiFdrKpiServiceApplication.java
@@ -0,0 +1,13 @@
+package it.pagopa.qi.fdrkpiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagopaQiFdrKpiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagopaQiFdrKpiServiceApplication.class, args);
+	}
+
+}
