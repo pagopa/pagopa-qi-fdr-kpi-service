3e374ff7207f559799369145382bead5048561bc
Dylan Tangredi <dylan.tangredi@bvtech.com>
2024-12-13 15:01:45 +0100

    [PPABV-92] feat: remove unnecessary date schema to optimize generated code; update param names in implemented method; add entity code examples;

--- src/main/kotlin/it/pagopa/qi/fdrkpi/controller/v1/FdrKpiController.kt
+++ src/main/kotlin/it/pagopa/qi/fdrkpi/controller/v1/FdrKpiController.kt
@@ -21,7 +21,7 @@ class FdrKpiController : FdrKpiApi {
      * @param period The time period granularity (single day or calendar month) (required)
      * @param date For daily KPIs: Specify the full date (YYYY-MM-DD). Must be at least 10 days
      *   before current date. For monthly KPIs: Specify year and month (YYYY-MM). (required)
-     * @param xPSPCode PSP code - Required only for broker queries. - If entityCode is a broker
+     * @param xPspCode PSP code - Required only for broker queries. - If entityCode is a broker
      *   fiscal code: this parameter is required - If entityCode is a PSP fiscal code: this
      *   parameter should not be provided (it would be a duplicate) (optional)
      * @return KPI calculated (status code 200) or Formally invalid input Possible error types: -
@@ -34,7 +34,7 @@ class FdrKpiController : FdrKpiApi {
         kpiType: String?,
         period: String?,
         date: String?,
-        xPSPCode: String?,
+        xPspCode: String?,
         exchange: ServerWebExchange?
     ): Mono<ResponseEntity<KPIResponseDto>> {
         val response =
