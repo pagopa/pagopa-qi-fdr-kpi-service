80651fa63132714c2575975f126d8b4363cb0242
Simone Infante <52280205+infantesimone@users.noreply.github.com>
2024-12-16 17:53:00 +0100

    fix: application-properties configuration

--- src/test/kotlin/it/pagopa/qi/fdrkpi/PagopaQiFdrKpiServiceApplicationTests.kt
+++ src/test/kotlin/it/pagopa/qi/fdrkpi/PagopaQiFdrKpiServiceApplicationTests.kt
@@ -2,9 +2,10 @@ package it.pagopa.qi.fdrkpi
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.TestPropertySource
 
 @SpringBootTest
-@TestPropertySource(locations = ["classpath:application-tests.properties"])
+@TestPropertySource(locations = ["classpath:application-test.properties"])
 class PagopaQiFdrKpiServiceApplicationTests {
 
     @Test fun contextLoads() {}
