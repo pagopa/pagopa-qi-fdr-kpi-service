cdbf88d85b1ff911d0f403035252b1a815c4a099
Dylan Tangredi <dylan.tangredi@bvtech.com>
2024-12-19 16:43:38 +0100

    [PPABV-97] feat: add logs to controller, config, query utils (INFO) and response builder (DEBUG)

--- src/main/kotlin/it/pagopa/qi/fdrkpi/controller/v1/FdrKpiController.kt
+++ src/main/kotlin/it/pagopa/qi/fdrkpi/controller/v1/FdrKpiController.kt
@@ -3,12 +3,15 @@ package it.pagopa.qi.fdrkpi.controller.v1
 import it.pagopa.generated.qi.fdrkpi.v1.api.FdrKpiApi
 import it.pagopa.generated.qi.fdrkpi.v1.model.*
 import it.pagopa.qi.fdrkpi.service.FdrKpiService
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RestController
 
 @RestController("FdrKpiV1Controller")
 class FdrKpiController(@Autowired private val fdrKpiService: FdrKpiService) : FdrKpiApi {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
     /**
      * GET /fdr-kpi/{kpiType}/{period}
      *
@@ -33,6 +36,20 @@ class FdrKpiController(@Autowired private val fdrKpiService: FdrKpiService) : Fd
         date: String,
         xPspCode: String?
     ): ResponseEntity<KPIResponseDto> {
+        val requesterInfo =
+            if (xPspCode != null) {
+                "Broker [$xEntityFiscalCode] (for PSP [$xPspCode])"
+            } else {
+                "PSP [$xEntityFiscalCode]"
+            }
+
+        logger.info(
+            "Received [{}] [{}] KPI request from [{}] for date [{}]",
+            period,
+            kpiType,
+            requesterInfo,
+            date
+        )
         val response = fdrKpiService.calculateKpi(xEntityFiscalCode, kpiType, period, date)
         return ResponseEntity.ok(response)
     }
